bf6aef6abb9170cd08e2fe6ec5dc25242e7be0e0
Kyle Reddoch <456477+kylereddoch@users.noreply.github.com>
2022-03-04 10:54:31 -0600

    Uses integers user_num and x as input, then outputs user_num divided by x 3 times.

---
+++ Lab1.21.py
@@ -0,0 +1,12 @@
+''' Type your code here. '''
+user_num = int(input())
+
+x = int(input())
+
+user_total = user_num // x
+
+result1 = user_total
+result2 = user_total // x
+result3 = result2 // x
+
+print(result1, result2, result3)
